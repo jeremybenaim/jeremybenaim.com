b9f5e5f3bb1c86daf30ee1f4b8ffc13a372d8c76
Jeremy Benaim <jeremybenaim@gmail.com>
2020-04-30 09:44:58 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -72,7 +72,7 @@ const Index = () => {
         <meta name="twitter:description" content={META.DESCRIPTION} />
       </Head>
       <article>
-        <p>Hi, I'm Jeremy Benaim. I build stuff for the internet.</p>
+        <p>Hi, I'm Jeremy. I build stuff for the internet.</p>
         <p>I've been a <strong>Senior Full Stack Javascript developer</strong> since 2008. My job is to help companies around the world to build successful products and deliver great user experiences.</p>
         <p>I have considerable expertise in Front End development with <strong>react</strong>, <strong>redux</strong>, <strong>nodejs</strong> and <strong>react-native</strong> and have worked with a number of different frameworks and languages throughout my career.</p>
         <p>I've been exclusively <strong>working remotely</strong> since 2016 and have been a supporter of the remote community since then.</p>
